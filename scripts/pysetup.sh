07b87de2b73819f9619cecd00fb1b6222c06704b
techupskills <82771267+techupskills@users.noreply.github.com>
2024-08-01 14:39:03 -0400

    Create pysetup.sh

---
+++ scripts/pysetup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+PYTHON_ENV=$1
+
+python3 -m venv ./$PYTHON_ENV  \
+        && export PATH=./$PYTHON_ENV/bin:$PATH \
+        && echo "source ./$PYTHON_ENV/bin/activate" >> ~/.bashrc
+
+source ./$PYTHON_ENV/bin/activate
+
+pip3 install -r /workspaces/diy-gen-ai/requirements/requirements.txt
