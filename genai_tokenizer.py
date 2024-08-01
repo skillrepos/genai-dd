38c98ae01d7dd9c63536f35f19d84bde7008608f
techupskills <82771267+techupskills@users.noreply.github.com>
2024-08-01 14:52:03 -0400

    Create genai_tokenizer.py

---
+++ genai_tokenizer.py
@@ -0,0 +1,16 @@
+import sys
+
+from transformers import AutoTokenizer
+
+model_name = sys.argv[1]
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+ 
+text = input("Enter text: ")
+tokenized_text = tokenizer.tokenize(text)
+print(tokenized_text)
+ 
+token_ids = tokenizer.encode(text)
+print(token_ids)
+ 
+tokens = tokenizer.convert_ids_to_tokens(token_ids)
+print(tokens)
