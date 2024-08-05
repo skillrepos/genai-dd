f109653cedf515378a0e5fe4396a51eea7d7e0e6
Brent Laster <bclaster@nclasters.org>
2024-08-05 17:25:34 -0400

    Update tokenizer.py
    Output formatting changes

--- genai/tokenizer.py
+++ genai/tokenizer.py
@@ -7,10 +7,13 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
  
 text = input("Enter text: ")
 tokenized_text = tokenizer.tokenize(text)
+print("")
 print(tokenized_text)
  
 token_ids = tokenizer.encode(text)
+print("")
 print(token_ids)
  
 tokens = tokenizer.convert_ids_to_tokens(token_ids)
+print("")
 print(tokens)
